fac140d761f56b4da52448f9990f73d7cafd5af2
Preston Doris <preston@prestondoris.com>
2018-04-24 14:00:08 -0700

    Create quickSortDuplicates
    quickSortDuplicates will remove all duplicates in the array while
    sorting returning a sorted array with only unique values.

--- sorts.py
+++ sorts.py
@@ -55,8 +55,7 @@ print ary
 print mergeSort(ary)
 
 
-
-def quicksort(array):
+def quickSortDuplicates(array):
 
     # set the last element of the array as the pivot
 
@@ -75,6 +74,37 @@ def quicksort(array):
                 pivotInd -= 1
             elif array[start] == pivot:
                 array.remove(array[start])
+            else:
+                start += 1
+    frontHalf = quicksort(array[:pivotInd])
+    backHalf = quicksort(array[pivotInd+1:])
+    array = frontHalf
+    array.append(pivot)
+    array += backHalf
+    return array
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print quicksort(test)
+
+
+
+def quickSort(array):
+
+    # set the last element of the array as the pivot
+
+    if array == [] or len(array) == 1:
+        return array
+    else:
+        pivot = array[-1]
+        pivotInd = len(ary) - 1
+        start = 0
+        while start < pivotInd:
+            if array[start] > pivot:
+                temp = array[start]
+                array[start] = array[pivotInd-1]
+                array[pivotInd-1] = pivot
+                array[pivotInd] = temp
+                pivotInd -= 1
             else:
                 start += 1
     frontHalf = quicksort(array[:pivotInd])
